16b3ce41c379f0e2fbb04068f089c6f94518fb57
Vladimir Korolev <balodja@gmail.com>
2020-07-09 09:38:37 +0300

    Add PCA to scaling algos. Add scaling to the AD script.

--- zwad/ad/preprocess.py
+++ zwad/ad/preprocess.py
@@ -8,9 +8,10 @@ def scale_values(features, algorithm=None):
     Parameters
     ----------
     features: Matrix of N-by-M values with N experiments and M features.
-    algorithm: Scaling algorithm: 'minmax' or 'std' (default).
+    algorithm: Scaling algorithm: 'minmax', 'std' or 'pca' (default).
     Minmax algorithm scales to the [0; 1] interval, while std algorithm
-    scales to the zero mean and unitary standard deviation.
+    scales to the zero mean and unitary standard deviation. PCA algorithm
+    performs linear transform to principal axes of covariance ellipsoid.
 
     Return
     ------
@@ -18,7 +19,10 @@ def scale_values(features, algorithm=None):
     """
     algorithm = algorithm or 'std'
 
-    if algorithm == 'minmax':
+    if algorithm == 'pca':
+        u, _, _ = np.linalg.svd(features, full_matrices=False)
+        return u
+    elif algorithm == 'minmax':
         minis = features.min(axis=0)
         maxis = features.max(axis=0)
         delta = maxis - minis
@@ -28,3 +32,5 @@ def scale_values(features, algorithm=None):
         mean = features.mean(axis=0)
         std = features.std(axis=0)
         return (features - mean) / np.maximum(std, np.finfo(np.float).eps)
+    else:
+        raise ValueError('Unkown scale algorithm: {}'.format(algorithm))
