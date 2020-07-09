4fa7881c2dec69e70bbf37becda14b918596bba4
Vladimir Korolev <balodja@gmail.com>
2020-07-09 13:29:17 +0300

    A bit of code for dataset loading.

--- zwad/ad/preprocess.py
+++ zwad/ad/preprocess.py
@@ -1,6 +1,31 @@
 import numpy as np
 
 
+def load_dataset(oid_file, feature_file, feature_names_file=None):
+    """
+    Load a dataset.
+
+    Parameters
+    ----------
+    oid_file: file with stored numpy array of oids
+    feature_file: file with stored numpy array of corresponding featues
+    feature_names_file: file with features' names (Optional, default: None).
+    """
+    oid = np.memmap(oid_file, mode='r', dtype=np.uint64)
+
+    if feature_names_file is not None:
+        with open(feature_names_file) as fo:
+            names = fo.read().split()
+
+        dt = [(name, np.float32) for name in names]
+        features = np.memmap(feature_file, mode='r', dtype=dt, shape=oid.shape)
+    else:
+        features = np.memmap(feature_file, mode='r', dtype=np.float32)
+        features = features.reshape(oid.size, -1)
+
+    return oid, features
+
+
 def scale_values(features, algorithm=None):
     """
     Scale the table of features.
