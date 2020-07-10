A bug in AD scaling.

zwad/ad/preprocess.py
@@ -8,7 +8,7 @@ def scale_values(features, algorithm=None):
     Parameters
     ----------
     features: Matrix of N-by-M values with N experiments and M features.
-    algorithm: Scaling algorithm: 'minmax', 'std' or 'pca' (default).
+    algorithm: Scaling algorithm: 'minmax', 'std'(default) or 'pca'.
     Minmax algorithm scales to the [0; 1] interval, while std algorithm
     scales to the zero mean and unitary standard deviation. PCA algorithm
     performs linear transform to principal axes of covariance ellipsoid.
@@ -19,8 +19,13 @@ def scale_values(features, algorithm=None):
     """
     algorithm = algorithm or 'std'
 
-    if algorithm == 'pca':
-        u, _, _ = np.linalg.svd(features, full_matrices=False)
+    if algorithm == 'std':
+        mean = features.mean(axis=0)
+        std = features.std(axis=0)
+        return (features - mean) / np.maximum(std, np.finfo(np.float).eps)
+    elif algorithm == 'pca':
+        mean = features.mean(axis=0)
+        u, _, _ = np.linalg.svd(features - mean, full_matrices=False)
         return u
     elif algorithm == 'minmax':
         minis = features.min(axis=0)
@@ -28,9 +33,5 @@ def scale_values(features, algorithm=None):
         delta = maxis - minis
         delta[delta == 0] = 1.0
         return (features - minis) / delta
-    elif algorithm == 'std':
-        mean = features.mean(axis=0)
-        std = features.std(axis=0)
-        return (features - mean) / np.maximum(std, np.finfo(np.float).eps)
     else:
         raise ValueError('Unkown scale algorithm: {}'.format(algorithm))