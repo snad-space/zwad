cf174eae888a48dd7a4c37901d4979d7aabb34f5
Konstantin Malanchev <hombit@gmail.com>
2022-10-05 09:48:40 -0500

    Add zwad.ad package to setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 setuptools.setup(name='zwad',
                  license_files = ('LICENSE',),
                  license='MIT',
-                 packages=['zwad'],
+                 packages=['zwad', 'zwad.ad'],
                  install_requires=[
                      'coniferest @ git+https://github.com/snad-space/coniferest.git@de3112ab0f2b5bea5d7d33887b2e119b1476840e#egg=coniferest-0.0.2',
                      'pandas<2.0',
